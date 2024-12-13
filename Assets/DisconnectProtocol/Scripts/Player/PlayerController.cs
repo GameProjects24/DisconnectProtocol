d055cb1faab4aad21df8d2dcca494f4a49905d3b
Ilya <ilya.starkov03@gmail.com>
2024-12-13 17:44:53 +0300

    Some fixes

--- Assets/DisconnectProtocol/Scripts/Player/PlayerController.cs
+++ Assets/DisconnectProtocol/Scripts/Player/PlayerController.cs
@@ -19,28 +19,6 @@ public class PlayerController : MonoBehaviour
 	[Tooltip("Acceleration and deceleration")]
 	public float SpeedChangeRate = 10.0f;
 
-	[Space(10)]
-	[Tooltip("The height the player can jump")]
-	public float JumpHeight = 1.2f;
-	[Tooltip("The character uses its own gravity value. The engine default is -9.81f")]
-	public float gravity = -15.0f;
-
-	[Space(10)]
-	[Tooltip("Time required to pass before being able to jump again. Set to 0f to instantly jump again")]
-	public float JumpTimeout = 0.1f;
-	[Tooltip("Time required to pass before entering the fall state. Useful for walking down stairs")]
-	public float FallTimeout = 0.15f;
-
-	[Header("Player Grounded")]
-	[Tooltip("If the character is grounded or not. Not part of the CharacterController built in grounded check")]
-	public bool Grounded = true;
-	[Tooltip("Useful for rough ground")]
-	public float GroundedOffset = -0.14f;
-	[Tooltip("The radius of the grounded check. Should match the radius of the CharacterController")]
-	public float GroundedRadius = 0.5f;
-	[Tooltip("What layers the character uses as ground")]
-	public LayerMask GroundLayers;
-
 	[Header("Cinemachine")]
 	[Tooltip("The follow target set in the Cinemachine Virtual Camera that the camera will follow")]
 	public GameObject CinemachineCameraTarget;
@@ -55,12 +33,6 @@ public class PlayerController : MonoBehaviour
 	// player
 	private float _speed;
 	private float _rotationVelocity;
-	private float _verticalVelocity;
-	private float _terminalVelocity = 53.0f;
-
-	// timeout deltatime
-	private float _jumpTimeoutDelta;
-	private float _fallTimeoutDelta;
 
 
 #if ENABLE_INPUT_SYSTEM
@@ -79,7 +51,7 @@ public class PlayerController : MonoBehaviour
 #if ENABLE_INPUT_SYSTEM
 			return _playerInput.currentControlScheme == "KeyboardMouse";
 #else
-				return false;
+			return false;
 #endif
 		}
 	}
@@ -102,29 +74,16 @@ public class PlayerController : MonoBehaviour
 #else
 			Debug.LogError( "Starter Assets package is missing dependencies. Please use Tools/Starter Assets/Reinstall Dependencies to fix it");
 #endif
-
-		// reset our timeouts on start
-		_jumpTimeoutDelta = JumpTimeout;
-		_fallTimeoutDelta = FallTimeout;
 	}
 
 	private void Update()
 	{
-		Gravity();
-		GroundedCheck();
 		Move();
 	}
 
 	private void LateUpdate()
 	{
 		CameraRotation();
-	}
-
-	private void GroundedCheck()
-	{
-		// set sphere position, with offset
-		Vector3 spherePosition = new Vector3(transform.position.x, transform.position.y - GroundedOffset, transform.position.z);
-		Grounded = Physics.CheckSphere(spherePosition, GroundedRadius, GroundLayers, QueryTriggerInteraction.Ignore);
 	}
 
 	private void CameraRotation()
@@ -193,55 +152,7 @@ public class PlayerController : MonoBehaviour
 		}
 
 		// move the player
-		_controller.Move(inputDirection.normalized * (_speed * Time.deltaTime) + new Vector3(0.0f, _verticalVelocity, 0.0f) * Time.deltaTime);
-	}
-
-	private void Gravity()
-	{
-		if (Grounded)
-		{
-			// reset the fall timeout timer
-			_fallTimeoutDelta = FallTimeout;
-
-			// stop our velocity dropping infinitely when grounded
-			if (_verticalVelocity < 0.0f)
-			{
-				_verticalVelocity = -2f;
-			}
-
-			// Jump
-			if (_input.jump && _jumpTimeoutDelta <= 0.0f)
-			{
-				// the square root of H * -2 * G = how much velocity needed to reach desired height
-				_verticalVelocity = Mathf.Sqrt(JumpHeight * -2f * gravity);
-			}
-
-			// jump timeout
-			if (_jumpTimeoutDelta >= 0.0f)
-			{
-				_jumpTimeoutDelta -= Time.deltaTime;
-			}
-		}
-		else
-		{
-			// reset the jump timeout timer
-			_jumpTimeoutDelta = JumpTimeout;
-
-			// fall timeout
-			if (_fallTimeoutDelta >= 0.0f)
-			{
-				_fallTimeoutDelta -= Time.deltaTime;
-			}
-
-			// if we are not grounded, do not jump
-			_input.jump = false;
-		}
-
-		// apply gravity over time if under terminal (multiply by delta time twice to linearly speed up over time)
-		if (_verticalVelocity < _terminalVelocity)
-		{
-			_verticalVelocity += gravity * Time.deltaTime;
-		}
+		_controller.SimpleMove(inputDirection.normalized * _speed);
 	}
 
 	private static float ClampAngle(float lfAngle, float lfMin, float lfMax)
@@ -249,17 +160,5 @@ public class PlayerController : MonoBehaviour
 		if (lfAngle < -360f) lfAngle += 360f;
 		if (lfAngle > 360f) lfAngle -= 360f;
 		return Mathf.Clamp(lfAngle, lfMin, lfMax);
-	}
-
-	private void OnDrawGizmosSelected()
-	{
-		Color transparentGreen = new Color(0.0f, 1.0f, 0.0f, 0.35f);
-		Color transparentRed = new Color(1.0f, 0.0f, 0.0f, 0.35f);
-
-		if (Grounded) Gizmos.color = transparentGreen;
-		else Gizmos.color = transparentRed;
-
-		// when selected, draw a gizmo in the position of, and matching radius of, the grounded collider
-		Gizmos.DrawSphere(new Vector3(transform.position.x, transform.position.y - GroundedOffset, transform.position.z), GroundedRadius);
 	}
 }
