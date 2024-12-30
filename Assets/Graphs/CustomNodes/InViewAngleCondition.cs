5f80b751fe415b159c5f85c08a328abb834f2f1b
BoreyTheTourist <borey_the_tourist@mail.ru>
2024-12-30 16:45:02 +0300

    Migrates to BehaviorGraph

---
+++ Assets/Graphs/CustomNodes/InViewAngleCondition.cs
@@ -0,0 +1,32 @@
+using System;
+using Unity.Behavior;
+using UnityEngine;
+
+[Serializable, Unity.Properties.GeneratePropertyBag]
+[Condition(name: "In view angle", story: "If angle between [self] and [target] [operator] [threshold]", category: "Conditions", id: "1579321f13324c849574f5bff137ccae")]
+public partial class InViewAngleCondition : Condition
+{
+    [SerializeReference] public BlackboardVariable<Transform> Self;
+    [SerializeReference] public BlackboardVariable<Transform> Target;
+    [Comparison(comparisonType: ComparisonType.All)]
+    [SerializeReference] public BlackboardVariable<ConditionOperator> Operator;
+    [SerializeReference] public BlackboardVariable<float> Threshold;
+
+    public override bool IsTrue()
+    {
+        if (Self == null || Target == null) {
+            return false;
+        }
+        var angle = Vector3.Angle(Self.Value.forward, Target.Value.position);
+        Debug.Log($"{angle}");
+        return ConditionUtils.Evaluate(angle, Operator, Threshold);
+    }
+
+    public override void OnStart()
+    {
+    }
+
+    public override void OnEnd()
+    {
+    }
+}
