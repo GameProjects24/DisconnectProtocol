a6c18845b563c5be9859ccf07dd90422730d3d13
BoreyTheTourist <borey_the_tourist@mail.ru>
2025-01-30 10:47:11 +0300

    Full switch to new MechaBody

--- Assets/DisconnectProtocol/Graphs/CustomNodes/Utilities/InitializeVariableAction.cs
+++ Assets/DisconnectProtocol/Graphs/CustomNodes/Utilities/InitializeVariableAction.cs
@@ -5,11 +5,12 @@ using Action = Unity.Behavior.Action;
 using Unity.Properties;
 
 [Serializable, GeneratePropertyBag]
-[NodeDescription(name: "Initialize Variable", story: "Initialize [var] with [tag]", category: "DisconnectProtocol/Actions", id: "b85bb8bf7fc652735347918ac7cff052")]
+[NodeDescription(name: "Initialize var", story: "Initialize [var] with [tag] , in children of [object] only", category: "DisconnectProtocol/Actions", id: "b85bb8bf7fc652735347918ac7cff052")]
 public partial class InitializeVariableAction : Action
 {
     [SerializeReference] public BlackboardVariable<GameObject> Var;
     [SerializeReference] public BlackboardVariable<string> Tag;
+    [SerializeReference] public BlackboardVariable<GameObject> Object;
 
     protected override Status OnStart()
     {
@@ -19,7 +20,11 @@ public partial class InitializeVariableAction : Action
     protected override Status OnUpdate()
     {
 		if (Var.Value == null) {
-			Var.Value = GameObject.FindGameObjectWithTag(Tag);
+			if (Object.Value != null) {
+				Var.Value = FindTransformWithTag(Object.Value.transform, Tag).gameObject;
+			} else {
+				Var.Value = GameObject.FindGameObjectWithTag(Tag);
+			}
 		}
         return Var.Value == null ? Status.Failure : Status.Success;
     }
@@ -27,5 +32,21 @@ public partial class InitializeVariableAction : Action
     protected override void OnEnd()
     {
     }
+
+	private Transform FindTransformWithTag(Transform parent, string tag) {
+		if (parent.CompareTag(tag)) {
+			return parent;
+		}
+		for (int i = 0; i < parent.childCount; ++i) {
+			Transform res = parent.GetChild(i);
+			if (res.CompareTag(tag)) {
+				return res;
+			}
+			if ((res = FindTransformWithTag(res, tag)) != null) {
+				return res;
+			}
+		}
+		return null;
+	}
 }
 
