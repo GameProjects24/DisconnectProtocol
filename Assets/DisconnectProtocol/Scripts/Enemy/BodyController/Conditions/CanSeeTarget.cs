66b9003fcb8f4c7eb7c4b1f175d2096161759441
BoreyTheTourist <borey_the_tourist@mail.ru>
2025-02-23 20:46:17 +0300

    Fix CanSeeTarget condition

--- Assets/DisconnectProtocol/Scripts/Enemy/BodyController/Conditions/CanSeeTarget.cs
+++ Assets/DisconnectProtocol/Scripts/Enemy/BodyController/Conditions/CanSeeTarget.cs
@@ -7,8 +7,6 @@ namespace DisconnectProtocol
         public float visionAngle;
 		private Transform m_eyes;
 
-		private bool m_wasSeen = false;
-
 		private CanSeeTarget() {}
 		public CanSeeTarget(Transform eyes, float visAngle) {
 			m_eyes = eyes;
@@ -18,15 +16,20 @@ namespace DisconnectProtocol
 		public bool Eval(Transform target) {
 			var dir = target.position - m_eyes.position;
 			bool inAngle = Vector3.Angle(m_eyes.forward, dir) <= visionAngle;
-			if (inAngle && !m_wasSeen) {
-				bool noObstacles = true;
-				if (Physics.Linecast(m_eyes.position, target.position, out var hit)) {
-					noObstacles = hit.transform.root.GetInstanceID() == target.GetInstanceID();
-				}
-				return m_wasSeen = noObstacles;
+			if (!inAngle) {
+				return false;
 			}
-
-			return inAngle;
+			
+			bool noObstacles = true;
+			if (Physics.Raycast(m_eyes.position, dir.normalized, out var hit)) {
+				var wh = hit.transform.GetComponentInParent<Whole>();
+				int hitId = hit.transform.GetInstanceID();
+				if (wh != null) {
+					hitId = wh.transform.GetInstanceID();
+				}
+				noObstacles = hitId == target.GetInstanceID();
+			}
+			return noObstacles;
 		}
     }
 }
