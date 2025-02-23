d726891639afbe03bd6e8ea3cfe85da75e3932b5
Ilya <ilya.starkov03@gmail.com>
2025-02-23 21:38:21 +0300

    Add loading indicator

---
+++ Assets/DisconnectProtocol/Scripts/UI/Loading.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class Loading : MonoBehaviour
+{
+    private RectTransform rectComponent;
+    private Image imageComp;
+    private bool up;
+
+    public float rotateTime = 1f;
+
+    public float startSize = 0.02f;
+    public float endSize = 0.8f;
+    public float openSpeed = 0.005f;
+    public float closeSpeed = 0.01f;
+
+    private void Start()
+    {
+        rectComponent = GetComponent<RectTransform>();
+        imageComp = rectComponent.GetComponent<Image>();
+        up = true;
+
+        // Вращение с помощью DOTween: плавное вращение с бесконечным повторением
+        rectComponent.DORotate(new Vector3(0f, 0f, 360f), rotateTime, RotateMode.FastBeyond360)
+            .SetLoops(-1, LoopType.Incremental)
+            .SetEase(Ease.Linear);
+    }
+
+    private void Update()
+    {
+        changeSize();
+    }
+
+    private void changeSize()
+    {
+        float currentSize = imageComp.fillAmount;
+
+        if (currentSize < endSize && up)
+        {
+            imageComp.DOFillAmount(imageComp.fillAmount + openSpeed, openSpeed);
+        }
+        else if (currentSize >= endSize && up)
+        {
+            up = false;
+        }
+        else if (currentSize >= startSize && !up)
+        {
+            imageComp.DOFillAmount(imageComp.fillAmount - closeSpeed, closeSpeed);
+        }
+        else if (currentSize < startSize && !up)
+        {
+            up = true;
+        }
+    }
+}
